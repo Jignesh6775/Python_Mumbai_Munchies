9b6c8f839728865492d2971f06268906b3fe2aa7
Jignesh <jigsvadiyatar6557@gmail.com>
2023-08-10 12:52:51 +0530

    New settings done

--- Django_CRUD/crud_project/crud_project/settings.py
+++ Django_CRUD/crud_project/crud_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!ffant9s39vp(#6wvtkvys&+cxrb98qvh7jom5s-q@q*e0k-m6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
